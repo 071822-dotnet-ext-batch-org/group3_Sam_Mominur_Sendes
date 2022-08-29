68eb61893789a031f7d650c66ccfcb32e6dec865
samba@SAMS_HP <Sosorbaram048@revature.net>
2022-08-29 11:01:40 -0700

    changed folder name

---
+++ SamsFolderforP2/ModelsLayer/DisplayDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelsLayer
+{
+    public class DisplayDto
+    {
+        public DisplayDto(string productName, string productDetails, decimal productPrice, int productInventory)
+        {
+            ProductName = productName;
+            ProductDetails = productDetails;
+            ProductPrice = productPrice;
+            ProductInventory = productInventory;
+        }
+
+        public string ProductName { get; set; }
+        public string ProductDetails { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductInventory { get; set; }
+
+    }
+}
