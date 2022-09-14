2f7168bc2a8709761a5ea61571e215b7ea5fbf34
(Sam) Sosorbaram Bayaraa <sosorbaram048@revature.net>
2022-09-14 12:51:08 -0700

    angular

---
+++ SamsFolderforP2/p2-group3-app/src/app/products.ts
@@ -0,0 +1,34 @@
+export interface Product {
+    id: number;
+    productName: string;
+    productPrice: number;
+    productDetail: string
+}
+
+export const products = [
+    {
+        id: 1
+        productName: 'Video1',
+        productPrice: 9,
+        productDeteail: 'about Video1'
+   
+    },
+
+    {
+        id: 2
+        productName: 'Video2',
+        productPrice: 5,
+        productDeteail: 'about Video2'
+   
+    },
+
+    {
+        id: 3
+        productName: 'Video3',
+        productPrice: 4,
+        productDeteail: 'about Video3'
+   
+    },
+
+   
+];
